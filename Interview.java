6863a758a9ba14ce0f9d463b848a765bc5b83514
evebue2001 <83917081+evebue2001@users.noreply.github.com>
2021-05-09 09:08:31 -0700

    Add files via upload

---
+++ Interview.java
@@ -0,0 +1,9 @@
+public class Interview {
+	public static void main(String[] args) { 
+		System.out.println("Person Interviewed:\tNayeli");
+		System.out.print("Nayeli's Hometown:\tStocking\n");
+		System.out.print("Nayeli's Major:\t\tChemistry\n");
+		System.out.println("Nayeli's Class Standing: First Year");
+		System.out.print("This is the second CSE20 Lab.");
+	}
+}
