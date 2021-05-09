86ca568c8bf625578307a0c6ba23d882db3b736e
evebue2001 <83917081+evebue2001@users.noreply.github.com>
2021-05-09 09:14:57 -0700

    Add files via upload

---
+++ Interviewer.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Interviewer {
+	public static void main(String[] args) {
+		
+		Scanner info = new Scanner(System.in);
+		
+		String name;
+		System.out.print("What is your name? ");
+		name = info.next();
+		System.out.println(name);
+		
+		
+		String hometown;
+		System.out.print("Where is your hometown? ");
+		hometown = info.next();
+		System.out.println(hometown);
+		
+		
+		String major;
+		System.out.print("What is your major? ");
+		major = info.next();
+		System.out.println(major);
+		
+		int standing;
+		System.out.print("What is the number of your class standing? ");
+		standing = info.nextInt();
+		System.out.println(standing + " yr");
+	}
+}
