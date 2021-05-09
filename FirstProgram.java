ec26a8ced31c2514717f6d3006cedd269bd62273
evebue2001 <83917081+evebue2001@users.noreply.github.com>
2021-05-09 09:02:21 -0700

    Add files via upload

---
+++ FirstProgram.java
@@ -0,0 +1,8 @@
+public class FirstProgram {
+	public static void main (String[] arguments) {
+		System.out.print ("Hello ");
+		System.out.println ("World!!");
+		System.out.print ("My name is Evelyn Buenabad. ");
+		System.out.println ("Welcome to CSE20 at UC Merced.");
+	}
+}
